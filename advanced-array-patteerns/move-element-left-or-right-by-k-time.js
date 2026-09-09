d26af363f68402882f886091703c3b30515405aa
aryan.uniqual <aryan.uniqual@gmail.com>
2026-09-09 22:41:50 +0530

    Refactor element movement logic to use reverse method for left and right shifts

--- advanced-array-patteerns/move-element-left-or-right-by-k-time.js
+++ advanced-array-patteerns/move-element-left-or-right-by-k-time.js
@@ -1,37 +1,79 @@
-//Move element left or right by k element
-// Suppose k is given 2 by user
+// //Move element left or right by k element
+// // Suppose k is given 2 by user
+//
+// console.log("Move element left or right by k element");
+// console.log("Suppose k is given 2 by user");
 
-console.log("Move element left or right by k element");
-console.log("Suppose k is given 2 by user");
+// let k = 2;
 
-const k = 2;
+// let arr = [1, 2, 3, 4, 5];
+// k = k%arr.length // which will give the accurate index value
+// console.log("arr: ", arr);
+
+// //Let move by - 2
+// for (let i = 0; i < k; i++) {
+//   let copy = arr[0];
+
+//   for (let j = 0; j < arr.length - 1; j++) {
+//     arr[j] = arr[j + 1];
+//   }
+
+//   arr[arr.length - 1] = copy;
+// }
+
+// console.log("left move output: ", arr);
+
+// //Right move by - 2
+// arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < k; i++) {
+//   let copy = arr[arr.length - 1];
+
+//   for (let j = arr.length - 1; j > 0; j--) {
+//     arr[j] = arr[j - 1];
+//   }
+
+//   arr[0] = copy;
+// }
+
+// console.log("right move output: ", arr);
+
 let arr = [1, 2, 3, 4, 5];
+let k = 2;
+k = k % arr.length;
 
-console.log("arr: ", arr);
-
-//Let move by - 2
-for (let i = 0; i < k; i++) {
-  let copy = arr[0];
-
-  for (let j = 0; j < arr.length - 1; j++) {
-    arr[j] = arr[j + 1];
+console.log("------- LEFT MOVE --------");
+console.log({ k });
+function reverse(i, j) {
+  while (i < j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
   }
-
-  arr[arr.length - 1] = copy;
 }
 
-console.log("left move output: ", arr);
+//1: reverse. -> 0,k-1 length
+reverse(0, k - 1);
 
-//Right move by - 2
+// //2: reverse. -> k,arr.length-1  length
+reverse(k, arr.length - 1);
+
+// //3: reverse. -> 0,arr.length-1  length
+reverse(0, arr.length - 1);
+
+console.log("LEFT MOVE OUTOUT: ", arr);
+console.log("------- RIGHT MOVE --------");
+console.log({ k });
 arr = [1, 2, 3, 4, 5];
-for (let i = 0; i < k; i++) {
-  let copy = arr[arr.length - 1];
 
-  for (let j = arr.length - 1; j > 0; j--) {
-    arr[j] = arr[j - 1];
-  }
+// 1. reverse ---> 0,arr.length -1
+reverse(0, arr.length - 1);
 
-  arr[0] = copy;
-}
+//2. reverse ----> 0, k-1
+reverse(0, k - 1);
 
-console.log("right move output: ", arr);
+//3 reverse ----> k, arr.length -1
+reverse(k, arr.length - 1);
+
+console.log("RIGHT MOVE OUTOUT: ", arr);
