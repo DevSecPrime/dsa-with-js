1bdf7e30b6a3d35fa0e3b3cdf3f5044c0f41db80
aryan.uniqual <aryan.uniqual@gmail.com>
2026-09-09 20:33:50 +0530

    Add implementation to move array elements to the left by one position

---
+++ advanced-array-patteerns/move-left-by-one.js
@@ -0,0 +1,15 @@
+// Move one element by one
+console.log("Move one element by one");
+const arr = [1, 2, 3, 4, 5];
+
+console.log("here is the original array: ", arr);
+
+let copy = arr[0];
+
+for (let i = 0; i < arr.length - 1; i++) {
+  arr[i] = arr[i + 1];
+}
+
+arr[arr.length - 1] = copy;
+
+console.log("Output: ", arr);
